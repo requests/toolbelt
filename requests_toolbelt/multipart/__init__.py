67676f7060545f0ecad622ef74ded60c955d0d25
Jay De Lanoy <desfido@gmail.com>
2014-03-10 11:40:35 -0400

    Created multipart module and moved multipart.py to multipart/encoder.py

---
+++ requests_toolbelt/multipart/__init__.py
@@ -0,0 +1,18 @@
+"""
+requests-toolbelt.multipart
+=================
+
+See http://toolbelt.rtfd.org/ for documentation
+
+:copyright: (c) 2014 by Ian Cordasco and Cory Benfield
+:license: Apache v2.0, see LICENSE for more details
+"""
+
+__title__ = 'requests-toolbelt'
+__authors__ = 'Ian Cordasco, Cory Benfield'
+__license__ = 'Apache v2.0'
+__copyright__ = 'Copyright 2014 Ian Cordasco, Cory Benfield'
+
+from .encoder import MultipartEncoder
+
+__all__ = [MultipartEncoder]
