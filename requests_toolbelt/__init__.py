4ef9e4376e725c80be4b327649718e780a8f787f
Ian Cordasco <graffatcolmingov@gmail.com>
2014-05-11 19:49:05 -0500

    My own cleverness is too clever for alpha versions

--- requests_toolbelt/__init__.py
+++ requests_toolbelt/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'requests-toolbelt'
 __authors__ = 'Ian Cordasco, Cory Benfield'
 __license__ = 'Apache v2.0'
 __copyright__ = 'Copyright 2014 Ian Cordasco, Cory Benfield'
-__version__ = '0.3.0a'
+__version__ = '0.3.0'
 __version_info__ = tuple(int(i) for i in __version__.split('.'))
 
 from .multipart import MultipartEncoder, MultipartDecoder
