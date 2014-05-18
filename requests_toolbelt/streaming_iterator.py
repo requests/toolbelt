2a00bf5f054bf17d764f7dad92cab78b68cf4d77
Ian Cordasco <graffatcolmingov@gmail.com>
2014-05-18 13:21:26 -0500

    Update StreamingIterator implementation
    Buffer bytes and return the number of bytes requested

--- requests_toolbelt/streaming_iterator.py
+++ requests_toolbelt/streaming_iterator.py
@@ -11,6 +11,8 @@ the size and stream the data without using a chunked transfer-encoding.
 
 """
 
+from .multipart.encoder import CustomBytesIO, encode_with
+
 
 class StreamingIterator(object):
 
@@ -20,21 +22,50 @@ class StreamingIterator(object):
 
     """
 
-    def __init__(self, size, iterator):
+    def __init__(self, size, iterator, encoding='utf-8'):
+        #: The expected size of the upload
         self.size = int(size)
+
         if self.size < 0:
             raise ValueError(
                 'The size of the upload must be a positive integer'
                 )
+
+        #: The iterator used to generate the upload data
         self.iterator = iterator
+
+        #: Encoding the iterator is using
+        self.encoding = encoding
+
+        # The buffer we use to provide the correct number of bytes requested
+        # during a read
+        self._buffer = CustomBytesIO()
 
     def __len__(self):
         return self.size
 
+    def _get_bytes(self):
+        try:
+            return encode_with(next(self.iterator), self.encoding)
+        except StopIteration:
+            return b''
+
+    def _load_bytes(self, size):
+        self._buffer.smart_truncate()
+        amount_to_load = size - len(self._buffer)
+        bytes_to_append = True
+
+        while amount_to_load > 0 and bytes_to_append:
+            bytes_to_append = self._get_bytes()
+            amount_to_load -= self._buffer.append(bytes_to_append)
+
     def read(self, size=-1):
+        size = int(size)
         if size == -1:
             return b''.join(self.iterator)
 
+        self._load_bytes(size)
+        return self._buffer.read(size)
         # Otherwise we're trying to read a specific size
         try:
             return next(self.iterator)
--- tests/test_streaming_iterator.py
+++ tests/test_streaming_iterator.py
@@ -10,12 +10,8 @@ class TestStreamingIterator(unittest.TestCase):
         self.size = 17
         self.uploader = StreamingIterator(self.size, self.iterator)
 
-    def test_read_returns_chunks(self):
-        for chunk in self.chunks:
-            assert self.uploader.read(8192) == chunk
-
     def test_read_returns_all_chunks_in_one(self):
-        assert self.uploader.read() == ''.join(self.chunks)
+        assert self.uploader.read() == b''.join(self.chunks)
 
     def test_read_returns_empty_string_after_exhausting_the_iterator(self):
         for i in range(0, 4):
@@ -23,3 +19,34 @@ class TestStreamingIterator(unittest.TestCase):
 
         assert self.uploader.read() == b''
         assert self.uploader.read(8192) == b''
+
+
+class TestStreamingIteratorWithLargeChunks(unittest.TestCase):
+    def setUp(self):
+        self.letters = ['a', 'b', 'c', 'd', 'e']
+        self.chunks = (letter * 2000 for letter in self.letters)
+        self.size = 5 * 2000
+        self.uploader = StreamingIterator(self.size, self.chunks)
+
+    def test_returns_the_amount_requested(self):
+        chunk_size = 1000
+        bytes_read = 0
+        while True:
+            b = self.uploader.read(chunk_size)
+            if not b:
+                break
+            assert len(b) == chunk_size
+            bytes_read += len(b)
+
+        assert bytes_read == self.size
+
+    def test_returns_all_of_the_bytes(self):
+        chunk_size = 8192
+        bytes_read = 0
+        while True:
+            b = self.uploader.read(chunk_size)
+            if not b:
+                break
+            bytes_read += len(b)
+
+        assert bytes_read == self.size
