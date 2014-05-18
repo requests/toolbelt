f09425300953216263500af6120b3a414a127e70
Ian Cordasco <graffatcolmingov@gmail.com>
2014-05-18 14:39:32 -0500

    Fill out StreamingIterator's docstring

--- requests_toolbelt/streaming_iterator.py
+++ requests_toolbelt/streaming_iterator.py
@@ -20,6 +20,30 @@ class StreamingIterator(object):
     This class provides a way of allowing iterators with a known size to be
     streamed instead of chunked.
 
+    In requests, if you pass in an iterator it assumes you want to use
+    chunked transfer-encoding to upload the data, which not all servers
+    support well. Additionally, you may want to set the content-length
+    yourself to avoid this but that will not work. The only way to preempt
+    requests using a chunked transfer-encoding and forcing it to stream the
+    uploads is to mimic a very specific interace. Instead of having to know
+    these details you can instead just use this class. You simply provide the
+    size and iterator and pass the instance of StreamingIterator to requests
+    via the data parameter like so:
+
+    .. code-block:: python
+
+        from requests_toolbelt import StreamingIterator
+
+        import requests
+
+        # Let iterator be some generator that you already have and size be
+        # the size of the data produced by the iterator
+
+        r = requests.post(url, data=StreamingIterator(size, iterator))
+
+    Naturally, you should also set the `Content-Type` of your upload
+    appropriately but the toolbelt will not attempt to guess that for you.
+
     """
 
     def __init__(self, size, iterator, encoding='utf-8'):
