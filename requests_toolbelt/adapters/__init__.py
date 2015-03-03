cca039f65561fa62d39154f61e0f8d5eb89b5713
Cory Benfield <lukasaoz@gmail.com>
2015-03-03 21:04:17 +0000

    Initial implementation of fingerprint adapter.

--- requests_toolbelt/adapters/__init__.py
+++ requests_toolbelt/adapters/__init__.py
@@ -11,5 +11,6 @@ See http://toolbelt.rtfd.org/ for documentation
 
 from .ssl import SSLAdapter
 from .source import SourceAddressAdapter
+from .fingerprint import FingerprintAdapter
 
-__all__ = ['SSLAdapter', 'SourceAddressAdapter']
+__all__ = ['SSLAdapter', 'SourceAddressAdapter', 'FingerprintAdapter']
---
+++ requests_toolbelt/adapters/fingerprint.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+requests_toolbelt.fingerprint_adapter
+=====================================
+
+This file contains an implementation of a Transport Adapter that validates
+the fingerprints of SSL certificates presented upon connection.
+"""
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
+
+class FingerprintAdapter(HTTPAdapter):
+    """
+    A HTTPS Adapter for Python Requests that verifies certificate fingerprints,
+    not just certificate hostnames.
+
+    Example usage:
+
+        >>> import requests
+        >>> import ssl
+        >>> from requests_toolbelt import FingerprintAdapter
+        >>> s = requests.Session()
+        >>> s.mount('https://twitter.com', FingerprintAdapter(twitter_fingerprint))
+
+    The fingerprint should be provided as a hexadecimal string, optionally
+    containing colons.
+    """
+
+    __attrs__ = HTTPAdapter.__attrs__ + ['fingerprint']
+
+    def __init__(self, fingerprint, **kwargs):
+        self.fingerprint = fingerprint
+
+        super(FingerprintAdapter, self).__init__(**kwargs)
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = PoolManager(num_pools=connections,
+                                       maxsize=maxsize,
+                                       block=block,
+                                       assert_fingerprint=self.fingerprint)
