a84a755a6b66e3972c03481beb021f72c6fee7ea
Ian Cordasco <graffatcolmingov@gmail.com>
2015-04-01 22:51:12 -0500

    Add more tests around Pools

--- tests/threaded/test_pool.py
+++ tests/threaded/test_pool.py
@@ -1,10 +1,12 @@
 """Module containing the tests for requests_toolbelt.threaded.pool."""
+import queue
 import unittest
 
 import mock
 import pytest
 
 from requests_toolbelt.threaded import pool
+from requests_toolbelt.threaded import thread
 
 
 class TestPool(unittest.TestCase):
@@ -49,3 +51,73 @@ class TestPool(unittest.TestCase):
         pool.Pool(None, num_processes=1, session=session)
         assert session.called is True
         session.assert_called_once_with()
+
+    def test_from_exceptions_populates_a_queue(self):
+        """Ensure a Queue is properly populated from exceptions."""
+        urls = ["https://httpbin.org/get?n={0}".format(n) for n in range(5)]
+        Exc = pool.ThreadException
+        excs = (Exc({'method': 'GET', 'url': url}, None) for url in urls)
+
+        job_queue = mock.MagicMock()
+        with mock.patch.object(queue, 'Queue', return_value=job_queue):
+            with mock.patch.object(thread, 'SessionThread'):
+                pool.Pool.from_exceptions(excs)
+
+        assert job_queue.put.call_count == 5
+        assert job_queue.put.mock_calls == [
+            mock.call({'method': 'GET', 'url': url})
+            for url in urls
+        ]
+
+    def test_from_urls_constructs_get_requests(self):
+        """Ensure a Queue is properly populated from an iterable of urls."""
+        urls = ["https://httpbin.org/get?n={0}".format(n) for n in range(5)]
+
+        job_queue = mock.MagicMock()
+        with mock.patch.object(queue, 'Queue', return_value=job_queue):
+            with mock.patch.object(thread, 'SessionThread'):
+                pool.Pool.from_urls(urls)
+
+        assert job_queue.put.call_count == 5
+        assert job_queue.put.mock_calls == [
+            mock.call({'method': 'GET', 'url': url})
+            for url in urls
+        ]
+
+    def test_from_urls_constructs_get_requests_with_kwargs(self):
+        """Ensure a Queue is properly populated from an iterable of urls."""
+        def merge(*args):
+            final = {}
+            for d in args:
+                final.update(d)
+            return final
+
+        urls = ["https://httpbin.org/get?n={0}".format(n) for n in range(5)]
+
+        kwargs = {'stream': True, 'headers': {'Accept': 'application/json'}}
+        job_queue = mock.MagicMock()
+        with mock.patch.object(queue, 'Queue', return_value=job_queue):
+            with mock.patch.object(thread, 'SessionThread'):
+                pool.Pool.from_urls(urls, kwargs)
+
+        assert job_queue.put.call_count == 5
+        assert job_queue.put.mock_calls == [
+            mock.call(merge({'method': 'GET', 'url': url}, kwargs))
+            for url in urls
+        ]
+
+    def test_join_all(self):
+        """Ensure that all threads are joined properly."""
+        session_threads = []
+
+        def _side_effect(*args, **kwargs):
+            thread = mock.MagicMock()
+            session_threads.append(thread)
+            return thread
+
+        with mock.patch.object(thread, 'SessionThread',
+                               side_effect=_side_effect):
+            pool.Pool(None).join_all()
+
+        for st in session_threads:
+            st.join.assert_called_once_with()
