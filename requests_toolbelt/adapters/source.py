c0792a3714a0eebfbd7e6e1cdc659c6246912a8e
Cory Benfield <lukasaoz@gmail.com>
2015-08-31 11:06:01 +0100

    Better documentation for the SourceAddressAdapter.

--- requests_toolbelt/adapters/source.py
+++ requests_toolbelt/adapters/source.py
@@ -17,6 +17,20 @@ class SourceAddressAdapter(HTTPAdapter):
     local address to bind to. This allows you to send your HTTP requests from a
     specific interface and IP address.
 
+    Two address formats are accepted. The first is a string: this will set the
+    local IP address to the address given in the string, and will also choose a
+    semi-random high port for the local port number.
+
+    The second is a two-tuple of the form (ip address, port): for example,
+    ``('10.10.10.10', 8999)``. This will set the local IP address to the first
+    element, and the local port to the second element. If ``0`` is used as the
+    port number, a semi-random high port will be selected.
+
+    .. warning:: Setting an explicit local port can have negative interactions
+                 with connection-pooling in Requests: in particular, it risks
+                 the possibility of getting "Address in use" errors. The
+                 string-only argument is generally preferred to the tuple-form.
+
     Example usage:
 
     .. code-block:: python
@@ -26,6 +40,7 @@ class SourceAddressAdapter(HTTPAdapter):
 
         s = requests.Session()
         s.mount('http://', SourceAddressAdapter('10.10.10.10'))
+        s.mount('https://', SourceAddressAdapter(('10.10.10.10', 8999))
     """
     def __init__(self, source_address, **kwargs):
         if isinstance(source_address, basestring):
