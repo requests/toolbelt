c8fb7b2e6aa547d0271dad6c29c24820693f12dd
Ryan Barrett <git@ryanb.org>
2016-05-08 08:52:53 -0700

    App Engine: set total timeout so urllib3 passes it to urlfetch
    Also adds the first test for AppEngineAdapter.
    
    Closes #145.
    
    Corresponding urllib3 code is https://github.com/snarfed/urllib3/blob/3239c172b19e41f41777db4bd55daae40c99b43d/urllib3/contrib/appengine.py#L184-193

---
+++ tests/test_appengine_adapter.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""Tests for the AppEngineAdapter."""
+import sys
+
+import mock
+import pytest
+import requests
+
+REQUESTS_SUPPORTS_GAE = requests.__build__ >= 0x021000
+
+if REQUESTS_SUPPORTS_GAE:
+    from requests.packages.urllib3.contrib import appengine as urllib3_appengine
+    from requests_toolbelt.adapters import appengine
+else:
+    appengine = urllib3_appengine = None
+
+
+@pytest.mark.skipif(sys.version_info >= (3,),
+                    reason="App Engine doesn't support Python 3 (yet)")
+@pytest.mark.skipif(not REQUESTS_SUPPORTS_GAE,
+                    reason="Requires Requests v2.10.0 or later")
+@mock.patch.object(urllib3_appengine, 'urlfetch')
+def test_get(mock_urlfetch):
+    """Tests a simple requests.get() call.
+
+    App Engine urlfetch docs:
+    https://cloud.google.com/appengine/docs/python/refdocs/google.appengine.api.urlfetch
+    """
+    response = mock.Mock(status_code=200, content='asdf', headers={})
+    mock_urlfetch.fetch = mock.Mock(return_value=response)
+
+    session = requests.Session()
+    session.mount('http://', appengine.AppEngineAdapter())
+    resp = session.get('http://url/', timeout=9, headers={'Foo': 'bar'})
+    assert resp.status_code == 200
+    assert resp.content == 'asdf'
+
+    args, kwargs = mock_urlfetch.fetch.call_args
+    assert args == ('http://url/',)
+    assert kwargs['deadline'] == 9
+    assert kwargs['headers']['Foo'] == 'bar'
